9ea43eb8336daa36f23540b13f4651de941fa628
Tai Doan <DoanTai11@gmail.com>
2024-01-15 20:01:17 +0000

    fixed

--- client/src/data.ts
+++ client/src/data.ts
@@ -22,42 +22,42 @@ if (localData) {
   data = JSON.parse(localData);
 }
 
-// export async function readEntries(): Promise<Entry[]> {
-//   const response = await fetch('/api/entries');
-//   if (!response.ok) {
-//     throw new Error(`fetch Error ${response.status}`);
-//   }
-//   return await response.json();
-// }
+export async function readEntries(): Promise<Entry[]> {
+  const response = await fetch('/api/entries');
+  if (!response.ok) {
+    throw new Error(`fetch Error ${response.status}`);
+  }
+  return await response.json();
+}
 
-// export async function addEntry(entry: UnsavedEntry): Promise<Entry[]> {
-//   const req = {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(entry),
-//   };
-//   const res = await fetch('/api/entries', req);
-//   if (!res.ok) {
-//     throw new Error(`fetch Error ${res.status}`);
-//   }
-//   return await res.json();
-// }
+export async function addEntry(entry: UnsavedEntry): Promise<Entry[]> {
+  const req = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(entry),
+  };
+  const res = await fetch('/api/entries', req);
+  if (!res.ok) {
+    throw new Error(`fetch Error ${res.status}`);
+  }
+  return await res.json();
+}
 
-// export async function updateEntry(entry: Entry): Promise<Entry[]> {
-//   const req = {
-//     method: 'PUT',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(entry),
-//   };
-//   const res = await fetch(`/api/entries/${entry.entryId}`, req);
-//   if (!res.ok) throw new Error(`fetch Error ${res.status}`);
-//   return await res.json();
-// }
+export async function updateEntry(entry: Entry): Promise<Entry[]> {
+  const req = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(entry),
+  };
+  const res = await fetch(`/api/entries/${entry.entryId}`, req);
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+  return await res.json();
+}
 
-// export async function removeEntry(entryId: number): Promise<void> {
-//   const req = {
-//     method: 'DELETE',
-//   };
-//   const res = await fetch(`/api/entries/${entryId}`, req);
-//   if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+export async function removeEntry(entryId: number): Promise<void> {
+  const req = {
+    method: 'DELETE',
+  };
+  const res = await fetch(`/api/entries/${entryId}`, req);
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
 }

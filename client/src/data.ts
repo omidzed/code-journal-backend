95edb1bee12ccec2dfd96fa5cf2194fe07124073
Tai Doan <DoanTai11@gmail.com>
2024-01-15 19:52:20 +0000

    completed code-journal

--- client/src/data.ts
+++ client/src/data.ts
@@ -30,26 +30,34 @@ export async function readEntries(): Promise<Entry[]> {
   return await response.json();
 }
 
-export function addEntry(entry: UnsavedEntry): Entry {
-  const newEntry = {
-    ...entry,
-    entryId: data.nextEntryId++,
+export async function addEntry(entry: UnsavedEntry): Promise<Entry[]> {
+  const req = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(entry),
   };
-  data.entries.unshift(newEntry);
-  return newEntry;
+  const res = await fetch('/api/entries', req);
+  if (!res.ok) {
+    throw new Error(`fetch Error ${res.status}`);
+  }
+  return await res.json();
 }
 
-export function updateEntry(entry: Entry): Entry {
-  const newEntries = data.entries.map((e) =>
-    e.entryId === entry.entryId ? entry : e
-  );
-  data.entries = newEntries;
-  return entry;
+export async function updateEntry(entry: Entry): Promise<Entry[]> {
+  const req = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(entry),
+  };
+  const res = await fetch(`/api/entries/${entry.entryId}`, req);
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+  return await res.json();
 }
 
-export function removeEntry(entryId: number): void {
-  const updatedArray = data.entries.filter(
-    (entry) => entry.entryId !== entryId
-  );
-  data.entries = updatedArray;
+export async function removeEntry(entryId: number): Promise<void> {
+  const req = {
+    method: 'DELETE',
+  };
+  const res = await fetch(`/api/entries/${entryId}`, req);
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
 }
--- server/server.ts
+++ server/server.ts
@@ -57,76 +57,84 @@ app.get('/api/entries/:id', async (req, res, next) => {
     next(err);
   }
 });
-// try {
-//   const id = +req.params.id;
-//   const data = await read();
-//   if (!(id in data.notes)) {
-//     res.status(404).json({ error: `${id} not found` });
-//     return;
-//   }
-//   res.json(data.notes[id]);
-// } catch (err) {
-//   console.error(err);
-//   res.status(500).json({ error: 'Unexpected error' });
-// }
 
-// app.post('/api/notes', async (req, res) => {
-//   try {
-//     const content: string = req.body.content;
-//     if (!content) {
-//       res.status(400).json({ error: 'Hey dummy, send some content!' });
-//       return;
-//     }
-//     const data = await read();
-//     const note = { id: data.nextId, content };
-//     data.notes[data.nextId] = note;
-//     data.nextId++;
-//     await write(data);
-//     res.status(201).json(note);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Unexpected error' });
-//   }
-// });
+app.post('/api/entries', async (req, res, next) => {
+  try {
+    const { title, notes, photoUrl } = req.body as Partial<Entry>;
+    if (!title || !notes || !photoUrl) {
+      throw new ClientError(
+        400,
+        'title, notes, and photoUrl are required fields'
+      );
+    }
+    const sql = `
+      insert into "entries" ("title", "notes", "photoUrl")
+        values ($1, $2, $3)
+        returning *;
+    `;
+    const params = [title, notes, photoUrl];
+    const result = await db.query<Entry>(sql, params);
+    const [entry] = result.rows;
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// app.put('/api/notes/:id', async (req, res) => {
-//   try {
-//     const content: string = req.body.content;
-//     if (!content) {
-//       res.status(400).json({ error: 'Hey dummy, send some content!' });
-//       return;
-//     }
-//     const data = await read();
-//     const id = +req.params.id;
-//     if (!(id in data.notes)) {
-//       res.status(404).json({ error: `${id} not found` });
-//       return;
-//     }
-//     data.notes[id].content = content;
-//     await write(data);
-//     res.status(201).json(data.notes[id]);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Unexpected error' });
-//   }
-// });
+app.put('/api/entries/:id', async (req, res, next) => {
+  try {
+    const entryId = +req.params.id;
+    const { title, notes, photoUrl } = req.body as Partial<Entry>;
+    if (!Number.isInteger(entryId) || !title || !notes || !photoUrl) {
+      throw new ClientError(
+        400,
+        'entryId, title, notes, and photoUrl are required fields'
+      );
+    }
+    const sql = `
+      update "entries"
+        set "title" = $1,
+            "notes" = $2,
+            "photoUrl" = $3
+        where "entryId" = $4
+        returning *;
+    `;
+    const params = [title, notes, photoUrl, entryId];
+    const result = await db.query<Entry>(sql, params);
+    const [entry] = result.rows;
+    if (!entry) {
+      throw new ClientError(404, `Entry with id ${entryId} not found`);
+    }
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// app.delete('/api/notes/:id', async (req, res) => {
-//   try {
-//     const data = await read();
-//     const id = +req.params.id;
-//     if (!(id in data.notes)) {
-//       res.status(404).json({ error: `${id} not found` });
-//       return;
-//     }
-//     delete data.notes[id];
-//     await write(data);
-//     res.sendStatus(204);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Unexpected error' });
-//   }
-// });
+app.delete('/api/entries/:id', async (req, res, next) => {
+  try {
+    const entryId = Number(req.params.id);
+    if (!Number.isInteger(entryId)) {
+      throw new ClientError(400, 'entryId must be an integer');
+    }
+    const sql = `
+      delete from "entries"
+        where "entryId" = $1
+        returning *;
+    `;
+    const params = [entryId];
+    const result = await db.query<Entry>(sql, params);
+    const [deleted] = result.rows;
+    if (!deleted) {
+      throw new ClientError(404, `Entry with id ${entryId} not found`);
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
